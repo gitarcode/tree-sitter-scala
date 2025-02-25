10cd93f54e770c58bace9ca41765a897bd4fb228
Ali Afroozeh <afroozeh@gmail.com>
2025-02-25 15:53:05 +0100

    Use import_spec name

--- grammar.js
+++ grammar.js
@@ -233,14 +233,14 @@ module.exports = grammar({
           optional(
             seq(
               ".",
-              choice(
-                field("wild_card", $.namespace_wildcard),
+              field("import_spec", choice(
+                $.namespace_wildcard,
                 $.namespace_selectors,
                 // Only allowed in Scala 3
                 // ImportExpr        ::=
                 //    SimpleRef {‘.’ id} ‘.’ ImportSpec |  SimpleRef ‘as’ id
                 $.as_renamed_identifier,
-              ),
+              )),
             ),
           ),
         ),
