e6d818a298186bf275c35f0d853456cd14800f8e
Ali Afroozeh <afroozeh@gmail.com>
2025-03-07 11:53:23 +0100

    Add explicit lambda_parameters node (#7)
    Add explicit lambda_parameters node

--- grammar.js
+++ grammar.js
@@ -73,13 +73,8 @@ module.exports = grammar({
     [$.match_expression],
     [$._given_constructor, $._type_identifier],
     [$.instance_expression],
-    // In case of: 'extension'  _indent  '{'  'case'  operator_identifier  'if'  operator_identifier  •  '=>'  …
-    // we treat `operator_identifier` as `simple_expression`
-    [$._simple_expression, $.lambda_expression],
     // 'package'  package_identifier  '{'  operator_identifier  •  ':'  …
     [$.self_type, $._simple_expression],
-    // 'package'  package_identifier  '{'  operator_identifier  '=>'  •  'enum'  …
-    [$.self_type, $.lambda_expression],
     // 'class'  _class_constructor  •  _automatic_semicolon  …
     [$._class_definition],
     // 'class'  operator_identifier  •  _automatic_semicolon  …
@@ -101,11 +96,13 @@ module.exports = grammar({
     [$.block, $._braced_template_body1],
     [$._simple_expression, $.self_type, $._type_identifier],
     [$._simple_expression, $._type_identifier],
-    [$.lambda_expression, $.self_type, $._type_identifier],
-    [$.lambda_expression, $._type_identifier],
     [$.binding, $._simple_expression, $._type_identifier],
     [$.val_declaration, $.identifier_pattern],
-    [$.var_declaration, $.identifier_pattern]
+    [$.var_declaration, $.identifier_pattern],
+    [$.self_type, $.lambda_parameters],
+    [$._simple_expression, $.lambda_parameters],
+    [$.self_type, $._type_identifier, $.lambda_parameters],
+    [$._type_identifier, $.lambda_parameters]
   ],
 
   word: $ => $._alpha_identifier,
@@ -1195,18 +1192,17 @@ module.exports = grammar({
       prec.right(
         seq(
           optional(seq(field("type_parameters", $.type_parameters), "=>")),
-          field(
-            "parameters",
-            choice(
-              $.bindings,
-              seq(optional("implicit"), $._identifier),
-              $.wildcard,
-            ),
-          ),
+          field("parameters", $.lambda_parameters),
           choice("=>", "?=>"),
           $._indentable_expression,
         ),
       ),
+
+    lambda_parameters: $ => choice(
+      $.bindings,
+      seq(optional("implicit"), field("single_parameter", $._identifier)),
+      $.wildcard,
+    ),
 
     /*
      *  ::=  [‘inline’] ‘if’ ‘(’ Expr ‘)’ {nl} Expr [[semi] ‘else’ Expr]
